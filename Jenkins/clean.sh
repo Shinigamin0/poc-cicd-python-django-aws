c04a855a50b4a56b6f89ac5f2c26f982b9eb5925
Omar Vargas <omar.vargas.cardenas@gmail.com>
2025-11-28 12:41:11 -0500

    Adición de comandos de limpieza
    Added additional Docker cleanup commands to remove all images, volumes, and unused build cache.

--- Jenkins/clean.sh
+++ Jenkins/clean.sh
@@ -3,3 +3,9 @@ docker stop $(docker ps -a -q)
 docker rm $(docker ps -a -q)
 docker system prune
 docker volume prune
+docker system prune -a --volumes -f
+docker rm -f $(docker ps -a -q)
+docker rmi -f $(docker images -a -q)
+docker volume rm $(docker volume ls -q)
+docker builder prune -a -f
+
